991c34131110bf0fd57eba035ea5931d6fc19e55
MeliaWijana <meliaa.wijana@gmail.com>
2025-07-20 00:18:33 +0800

    Update coin_page.py

--- public/coin_page.py
+++ public/coin_page.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="EcoSort - Coins", layout="wide")
 st.title("🪙 Coin Info")
 
-with open("public/coin.html", "r", encoding="utf-8") as f:
+with open("coin.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 
 components.html(html_code, height=1000, scrolling=True)
