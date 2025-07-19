e2f22210283a32f82df28e46bd6d880abe66af40
MeliaWijana <meliaa.wijana@gmail.com>
2025-07-19 23:37:04 +0800

    Update coin_page.py

--- pages/coin_page.py
+++ pages/coin_page.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.title("💰 e-Wallet & Investasi")
-st.info("Fitur ini masih dalam pengembangan.")
+st.set_page_config(page_title="EcoSort - Coins", layout="wide")
+st.title("🪙 Coin Info")
+
+with open("public/coin.html", "r", encoding="utf-8") as f:
+    html_code = f.read()
+
+components.html(html_code, height=1000, scrolling=True)
