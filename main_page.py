91a7f9302922057d7e0ae56866192a1970bbf958
MeliaWijana <meliaa.wijana@gmail.com>
2025-07-20 18:46:38 +0800

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,11 +1,55 @@
+#only for vlm show
+'''
+import streamlit as st
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from modul.model_vlm import classify_image_from_file
+import tempfile
+from PIL import Image
+
+hf_token = st.secrets["HF_TOKEN"]
+
+uploaded_file = st.file_uploader("Upload gambar")
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp:
+        image.save(temp.name)
+        result = classify_image_from_file(temp.name, token=hf_token)
+    st.write(f"Hasil klasifikasi: {result}")
+
+'''
+
+#only for llm
+'''
+import streamlit as st
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from modul.model_llm import get_bot_reply
+
+st.set_page_config(page_title="Chatbot Satvika", page_icon="💬")
+st.title("🤖 Chatbot Satvika (Streamlit Edition)")
+
+# Inisialisasi riwayat obrolan
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# Input dari user
+user_input = st.text_input("Tanya ke chatbot:")
+
+# Proses jawaban jika ada input
+if user_input:
+    with st.spinner("Bot sedang menjawab..."):
+        reply = get_bot_reply(user_input)
+        st.session_state.chat_history.append(("🧑 Kamu", user_input))
+        st.session_state.chat_history.append(("🤖 Bot", reply))
+
+# Tampilkan riwayat obrolan
+for speaker, text in st.session_state.chat_history:
+    st.markdown(f"**{speaker}:** {text}")
+'''
+
 import streamlit as st
 import streamlit.components.v1 as components
 import os
 import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from modul.model_vlm import classify_image_from_file
-from modul.model_llm import get_bot_reply
 
 st.set_page_config(page_title="EcoSort UI", layout="wide")
 st.title("🌱 EcoSort Interface")
