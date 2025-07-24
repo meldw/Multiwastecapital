3584a8834a439a1470e5f409e5418e1376a76d6c
MeliaWijana <meliaa.wijana@gmail.com>
2025-07-24 23:44:50 +0800

    Update model_llm.py

--- modul/model_llm.py
+++ modul/model_llm.py
@@ -1,3 +1,17 @@
+# Inisialisasi pipeline dengan model dari Hugging Face
+pipe = pipeline(
+    "text-generation",
+    model="satvikag/chatbot",
+    max_new_tokens=100,
+    do_sample=True,
+    temperature=0.7,
+)
+
+def get_bot_reply(user_input):
+    prompt = user_input
+    result = pipe(prompt)
+    reply = result[0]["generated_text"].replace(prompt, "").strip()
+    return reply
 '''
 from transformers import pipeline
 
